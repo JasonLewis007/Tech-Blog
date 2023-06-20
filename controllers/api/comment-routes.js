5bf3539cebf75b05d4306301d315b06c3a69e3fb
Jason Lewis <pensfan5050@gmail.com>
2023-06-19 20:42:37 -0400

    added routes to controllers, api.  added code to html.  addede code to models

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,29 @@
+// get all comments
+router.get('/', (req, res) => {
+
+});
+
+
+// post a comment
+router.post('/', (req, res) => {
+  if (req.session) {
+    Comment.create({
+      comment_text: req.body.comment_text,
+      post_id: req.body.post_id,
+      user_id: req.session.user_id
+    })
+      .then(dbCommentData => res.json(dbCommentData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  }
+});
+
+
+// delete a comment
+router.delete('/:id', (req, res) => {
+
+});
+
+module.exports = router
