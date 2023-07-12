9a03040377a8f14e14c988532e1abdf25f5cec3a
Jason Lewis <pensfan5050@gmail.com>
2023-07-12 13:57:28 -0400

    changed code in javascript folder

--- Models/index.js
+++ Models/index.js
@@ -2,7 +2,7 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
-//create associations
+// create associations
 User.hasMany(Post, {
     foreignKey: 'user_id'
 });
@@ -13,18 +13,19 @@ Post.belongsTo(User, {
 
 Comment.belongsTo(User, {
     foreignKey: 'user_id'
-  });
-  
+});
+
 Comment.belongsTo(Post, {
     foreignKey: 'post_id'
 });
-  
+
 User.hasMany(Comment, {
     foreignKey: 'user_id'
 });
-  
+
 Post.hasMany(Comment, {
     foreignKey: 'post_id'
 });
 
-module.exports = {User, Post, Comment};
+
+module.exports = { User, Post, Comment };
